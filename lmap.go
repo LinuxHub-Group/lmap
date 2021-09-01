223f03beac6a9926485fb2e8e060d51d461713a9
sswolf <578357612@qq.com>
2021-09-01 09:34:18 +0800

    初始提交

---
+++ lmap.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+var icmp ICMP
+
+var wg sync.WaitGroup
+
+type ICMP struct {
+	Type        uint8
+	Code        uint8
+	Checksum    uint16
+	Identifier  uint16
+	SequenceNum uint16
+}
+
+func main() {
+	wg.Add(1)
+	go CheckIP()
+	CheckIP()
+	wg.Wait()
+}
+
+func CheckIP() {
+	defer wg.Done()
+	var use []string
+	var notuse []string
+	t := time.Now()
+	hosts, _ := hosts("10.150.1.1/24")
+	for _, ip := range hosts {
+		tmp := ip
+		go func(data string) {
+			bool := ping(data)
+			if bool {
+				use = append(use, data)
+				fmt.Println("已使用IP：", use)
+			} else {
+				notuse = append(notuse, data)
+				fmt.Println("未使用IP：", notuse)
+			}
+		}(tmp)
+	}
+	wg.Wait()
+	elapsed := time.Since(t)
+	fmt.Println("IP扫描完成,耗时", elapsed)
+}
+
+func hosts(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+func ping(ip string) bool {
+	//开始填充数据包
+	icmp.Type = 8 //8->echo message  0->reply message
+	icmp.Code = 0
+	icmp.Checksum = 0
+	icmp.Identifier = 0
+	icmp.SequenceNum = 0
+
+	recvBuf := make([]byte, 32)
+	var buffer bytes.Buffer
+
+	//先在buffer中写入icmp数据报求去校验和
+	binary.Write(&buffer, binary.BigEndian, icmp)
+	icmp.Checksum = CheckSum(buffer.Bytes())
+	//然后清空buffer并把求完校验和的icmp数据报写入其中准备发送
+	buffer.Reset()
+	binary.Write(&buffer, binary.BigEndian, icmp)
+
+	Time, _ := time.ParseDuration("1s")
+	conn, err := net.DialTimeout("ip4:icmp", ip, Time)
+	if err != nil {
+		return false
+	}
+	_, err = conn.Write(buffer.Bytes())
+	if err != nil {
+		log.Println("conn.Write error:", err)
+		return false
+	}
+	conn.SetReadDeadline(time.Now().Add(time.Second * 2))
+	num, err := conn.Read(recvBuf)
+	if err != nil {
+		return false
+	}
+
+	conn.SetReadDeadline(time.Time{})
+
+	if string(recvBuf[0:num]) != "" {
+		return true
+	}
+	return false
+
+}
+
+func CheckSum(data []byte) uint16 {
+	var (
+		sum    uint32
+		length int = len(data)
+		index  int
+	)
+	for length > 1 {
+		sum += uint32(data[index])<<8 + uint32(data[index+1])
+		index += 2
+		length -= 2
+	}
+	if length > 0 {
+		sum += uint32(data[index])
+	}
+	sum += (sum >> 16)
+
+	return uint16(^sum)
+}
